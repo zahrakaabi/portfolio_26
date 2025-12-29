135db3800d1f5409999ea2437cf8356680a76e5d
zahrakaabi <kaabizahra@gmail.com>
2025-12-29 08:58:38 +0100

    feat: Update project mock data and improve styling for project details

--- src/_mocks/projects-mock.ts
+++ src/_mocks/projects-mock.ts
@@ -9,6 +9,7 @@ import { ProjectType } from "@/types/shared";
 /* -------------------------------------------------------------------------- */
 export const projects: ProjectType[] = [
     //ajouter eskiss
+    //add this portfolio
     {
         $id: "01",
         title_1: "Thunder",
@@ -33,19 +34,7 @@ export const projects: ProjectType[] = [
         techs: "React Js | Local Storage",
         description: 'This Pomodoro timer website is built as a Progressive Web App (PWA), allowing users to install it on their devices for an app-like experience via "Add to Home Screen." It offers a customizable focus session with the ability to choose between three fonts, three color themes, and set custom durations for Pomodoro, short break, and long break sessions. User preferences are saved using local storage, ensuring a personalized and consistent experience across visits.'
     },
-    {
-        $id: "06",
-        title_1: "Youtube",
-        title_2: " Clone",
-        image: "youtube.png",
-        video: 'ytb clone.mp4',
-        details: "Development",
-        src:"https://github.com/zahrakaabi/YOUTUBE_CLONE",
-        color: "#b1a994",
-        techs: "React Js | Material UI | Rapid API",
-        description: "This project is a video streaming platform built with React, Material UI and RapidApi, offering core functionalities such as video playback, search, channel browsing, comments, and recommendations. It features responsive design for optimal viewing across devices, dynamic routing for video and channel pages. Users can enjoy a smooth, intuitive UI that closely mirrors the YouTube experience."
-    },
-    {
+    /*{
         $id: "02",
         title_1: "Boho",
         title_2: "",
@@ -56,30 +45,7 @@ export const projects: ProjectType[] = [
         color: "#e9eaec",
         techs: "Vanilla Javascript | Html | CSS",
         description: "An e-commerce website developed using Vanilla JavaScript, HTML, and SCSS, allows users to explore a variety of beautifully arranged floral products, view detailed descriptions, and complete purchases through a smooth and intuitive interface. After completing a checkout, customers have the option to download a PDF receipt of their order, enhancing the overall shopping experience with a touch of professionalism."
-    },
-    {
-        $id: "07",
-        title_1: "Custom product de",
-        title_2: "tails",
-        image: 'screen-capture-4.gif',
-        details: "Development",
-        src:"https://github.com/zahrakaabi/CUSTOM_PRODUCTS_AUTO_SLIDER",
-        color: "#e9eaec",
-        techs: "Vanilla Javascript | Html | CSS",
-        description: "A custom component that features a custom-built auto slider that showcases multiple products in an interactive carousel. It automatically transitions between images. This component highlights product visuals effectively, enhancing the overall presentation and helping users explore items in more detail."
-    },
-    {
-        $id: "04",
-        title_1: "Obse",
-        title_2: "ssion",
-        image: "obsession.png",
-        video: 'video2.mp4',
-        details: "Design & Build",
-        src:"https://github.com/zahrakaabi13/Chef-d-oeuvre",
-        color: "#b1a994",
-        techs: "React JS | Node Js",
-        description: "A printed hoodie e-commerce website was fully designed and developed to offer a smooth shopping experience. Built React Js and Node Js, it allows users to browse a variety of hoodie designs, view detailed product pages, and complete purchases."
-    },
+    },*/
     {
         $id: "03",
         title_1: "Movie",
@@ -93,6 +59,41 @@ export const projects: ProjectType[] = [
         description: "This movie app, fully designed and developed using React.js and Node.js, offers a seamless experience to browse, search, and manage movies. Users can easily search for movies, add or remove them from their favorites, and access a personalized dashboard with full CRUD (Create, Read, Update, Delete) capabilities for managing movie entries."
     },
     {
+        $id: "07",
+        title_1: "Custom product de",
+        title_2: "tails",
+        image: 'screen-capture-4.gif',
+        details: "Development",
+        src:"https://github.com/zahrakaabi/CUSTOM_PRODUCTS_AUTO_SLIDER",
+        color: "#e9eaec",
+        techs: "Vanilla Javascript | Html | CSS",
+        description: "A custom component that features a custom-built auto slider that showcases multiple products in an interactive carousel. It automatically transitions between images. This component highlights product visuals effectively, enhancing the overall presentation and helping users explore items in more detail."
+    },
+    /*{
+        $id: "04",
+        title_1: "Obse",
+        title_2: "ssion",
+        image: "obsession.png",
+        video: 'video2.mp4',
+        details: "Design & Build",
+        src:"https://github.com/zahrakaabi13/Chef-d-oeuvre",
+        color: "#b1a994",
+        techs: "React JS | Node Js",
+        description: "A printed hoodie e-commerce website was fully designed and developed to offer a smooth shopping experience. Built React Js and Node Js, it allows users to browse a variety of hoodie designs, view detailed product pages, and complete purchases."
+    },*/
+    {
+        $id: "06",
+        title_1: "Youtube",
+        title_2: " Clone",
+        image: "youtube.png",
+        video: 'ytb clone.mp4',
+        details: "Development",
+        src:"https://github.com/zahrakaabi/YOUTUBE_CLONE",
+        color: "#b1a994",
+        techs: "React Js | Material UI | Rapid API",
+        description: "This project is a video streaming platform built with React, Material UI and RapidApi, offering core functionalities such as video playback, search, channel browsing, comments, and recommendations. It features responsive design for optimal viewing across devices, dynamic routing for video and channel pages. Users can enjoy a smooth, intuitive UI that closely mirrors the YouTube experience."
+    },
+    /*{
         $id: "05",
         title_1: "Watch ",
         title_2: "& Go",
@@ -103,7 +104,7 @@ export const projects: ProjectType[] = [
         color: "#dcd0c2",
         techs: "Vanilla Javscript | Html | CSS",
         description: "Ane-commerce website that sells watches, it was fully designed and developed to offer a smooth shopping experience. Built Vanilla Js no frameworks, it allows users to browse a variety of hoodie designs, view detailed product pages, and complete purchases."
-    },
+    },*/
     {
         $id: "08",
         title_1: "Sharingan",
